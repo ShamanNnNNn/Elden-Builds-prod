6a1a262c1c004f70471a96e058d333cbacaf6c89
Кузнец <dimapokazeev27@gmail.com>
2026-05-08 14:36:59 +0300

    Delete src directory

--- src/main/java/com/example/demo/controller/OrderNotFoundExeption.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.controller;
-
-public class OrderNotFoundExeption extends RuntimeException {
-    public OrderNotFoundExeption(Long id) {
-        super("Задача с ID " + id + " не найдена");
-    }
-}
